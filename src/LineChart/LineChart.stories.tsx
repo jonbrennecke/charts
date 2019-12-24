60722685ee4a6c2da81c6b3dd049800a87f3f109
Jon Brennecke <jpbrennecke@gmail.com>
2019-12-23 18:51:37 -0800

    basic line chart

--- src/LineChart/LineChart.stories.tsx
+++ src/LineChart/LineChart.stories.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import uuid from 'uuid';
+import { scaleLinear } from 'd3-scale';
+import { range } from 'd3-array';
+import { randomUniform } from 'd3-random';
 import { LineChart } from './LineChart';
 
+const dimensions = {
+  height: 240,
+  width: 320,
+};
+
+const numberOfDataPoints = 10;
+const yDomain = [0, 100];
+const xDomain = [0, numberOfDataPoints];
+
+const xScale = scaleLinear()
+  .domain(xDomain)
+  .range([0, dimensions.width]);
+const yScale = scaleLinear()
+  .domain(yDomain)
+  .range([dimensions.height, 0]);
+
+const random = randomUniform(xDomain[0], xDomain[1]);
+const values = range(yDomain[1]).map(random);
+
+const chartData = values.map((y, i) => ({ id: uuid.v4(), y, x: i }));
+
 storiesOf('Charts', module).add('Line chart', () => (
-  <div>
-    <LineChart />
+  <div style={{ width: 320, height: 240 }}>
+    <LineChart data={chartData} xScale={xScale} yScale={yScale} />
   </div>
 ));
--- src/LineChart/LineChart.tsx
+++ src/LineChart/LineChart.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { ScaleLinear } from 'd3-scale';
+import { line } from 'd3-shape';
+import property from 'lodash/property';
 
-export interface ILineChartProps {}
-
-export const LineChart: React.SFC<ILineChartProps> = () => (
-  <div data-test="line-chart">
-    
-  </div>
-);
+export interface ILineChartProps<T = any> {
+  data: T[];
+  xScale: ScaleLinear<number, number>;
+  yScale: ScaleLinear<number, number>;
+  xValueAccessor?(data: T): number;
+  yValueAccessor?(data: T): number;
+}
+export const LineChart = <T extends any = { x: number; y: number }>({
+  data,
+  xScale,
+  yScale,
+  xValueAccessor = property('x'),
+  yValueAccessor = property('y'),
+}: ILineChartProps<T>) => {
+  const [x0, x1] = xScale.range();
+  const [y1, y0] = yScale.range();
+  const lineGenerator = line<T>()
+    .x(d => xScale(xValueAccessor(d)))
+    .y(d => yScale(yValueAccessor(d)));
+  return (
+    <div data-test="line-chart">
+      <svg
+        width={x1}
+        height={y1}
+        viewBox={`${x0} ${y0} ${x1} ${y1}`}
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+      >
+        <path
+          d={lineGenerator(data) || ''}
+          stroke="#ff0000"
+          fill="transparent"
+        />
+      </svg>
+    </div>
+  );
+};
