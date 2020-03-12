766ae55a40307dc73e9a9a9168af5c802eb28368
Jonathan Brennecke <jpbrennecke@gmail.com>
2020-03-12 12:46:18 -0700

    added vertical gridlines

--- src/components/chart/GridLines/GridLines.tsx
+++ src/components/chart/GridLines/GridLines.tsx
@@ -5,7 +5,8 @@ import { ColorTheme, colorThemes } from '../../../theme';
 export interface IGridLinesProps {
   numberOfYTicks: number;
   yScale: ScaleLinear<number, number>;
-  xScale: ScaleLinear<number, number> | ScaleBand<number>;
+  // TODO: xScale: ScaleLinear<number, number> |
+  xScale: ScaleBand<number>;
   colorTheme?: ColorTheme;
 }
 
@@ -16,10 +17,13 @@ export const GridLines = ({
   colorTheme = colorThemes.light,
 }: IGridLinesProps) => {
   const [x0, x1] = xScale.range();
+  const [y1, y0] = yScale.range();
+  const xDomain = xScale.domain();
+  const bandWidth = xScale.bandwidth();
   return (
     <g data-test="gridlines">
       {yScale.ticks(numberOfYTicks).map(n => (
-        <g key={n}>
+        <g key={`y-gridline-${n}`}>
           <line
             x1={x0}
             x2={x1}
@@ -30,6 +34,22 @@ export const GridLines = ({
           />
         </g>
       ))}
+
+      {xDomain.map(i => {
+        const bandCenter = (xScale(i) || 0) + bandWidth / 2;
+        return (
+          <g key={`x-gridline-${i}`}>
+            <line
+              x1={bandCenter}
+              x2={bandCenter}
+              y1={y0}
+              y2={y1}
+              stroke={colorTheme.components.chart.axis.gridline.stroke}
+              fill="transparent"
+            />
+          </g>
+        );
+      })}
     </g>
   );
 };
