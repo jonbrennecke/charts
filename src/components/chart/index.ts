e448ce35c4a2cd76f9b6181c6f4ce09e8874c20a
Jonathan Brennecke <jpbrennecke@gmail.com>
2020-03-26 12:54:32 -0700

    deleted unnecessary AreaChart

--- src/components/chart/index.ts
+++ src/components/chart/index.ts
@@ -1,6 +1,5 @@
 export * from './LineChart';
 export * from './BarChart';
-export * from './AreaChart';
 export * from './PieChart';
 export * from './ChartDimensions';
 export * from './ChartHeader';
